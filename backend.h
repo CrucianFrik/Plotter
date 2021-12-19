75cfdf880df743db95829844edc1eb77c71a6a7d
Sirius <andreymarkin20042015@gmail.com>
2021-12-19 18:22:36 +0300

    Revert "compiling version of backend; requests improvements"
    This reverts commit f2fd4437a45cb4584f007ffa7e509d4879a38a86.

--- backend.h
+++ backend.h
@@ -1,11 +1,6 @@
 #include <vector>
 #include <cmath>
-#define DEBUG_OUT(x) std::cout <<__LINE__<<": "<< (#x) <<" = "<< (x) << std::endl
 
-bool isnan(double n)
-{
-	return (n != n);
-}
 
 struct MyExeption
 {
@@ -28,7 +23,7 @@ struct Steps
 	{}
 };
 
-const Steps step(1, 0.1, 0.1);
+const Steps step(1, 1, 1);
 
 struct ScreenPoint
 {
@@ -53,11 +48,8 @@ struct Point
 		: x{x1}, y{x2}, z{x3} 
 	{}
 
-	Point(const Point& p)
-		:x{p.x}, y{p.y}, z{p.z} 
-	{}
-
 };
+
 
 struct Range
 {
@@ -74,14 +66,12 @@ struct function
 
 	double operator()(Point p)
 	{
-		return pow(p.x, 2)/16 + pow(p.y, 2)/25 - pow(p.z, 2)/13;
-		//return pow(x, 2) + pow(y, 2) + pow(z, 2) - 25;
+		return pow(p.x, 2) + pow(p.y, 2) + pow(p.z, 2) - 25;
 	}
 
 	double operator()(double x, double y, double z)
 	{
-		return pow(x, 2)/16 + pow(y, 2)/25 - pow(z, 2)/13;
-		//return pow(x, 2) + pow(y, 2) + pow(z, 2) - 25;
+		return pow(x, 2) + pow(y, 2) + pow(z, 2) - 25;
 	}
 };
 
@@ -90,52 +80,46 @@ class CalcFunction
 private:
 	function F;
 	Range R;
-	double error_rate = step.f/10;
+	double error_rate = step.f/2;
 
 public:
 	CalcFunction(function FF, Range rr)
 		: F{FF}, R{rr} {}
 
-	/*bool check_point(const Point* p)
+	bool check_point(const Point* p)
 	{
-		if ((p->x && p->y && (isnan(p->z))) || (p->y && p->z && (isnan(p->x))) || (p->x && p->z && (isnan(p->y))))
+		if ((p->x && p->y && (!p->z)) || (p->y && p->z && (!p->x)) || (p->x && p->z && (!p->y)))
 			return false;
 		return true;
-	}*/ //DEBUG comment	
+	}	
 
 	Point put_to_nan(const Point* p, double arg)
 	{
-		if (isnan(p->x))
+		if (!p->x)
 			return Point(arg, p->y, p->z);
-		if (isnan(p->y))
+		if (!p->y)
 			return Point(p->x, arg, p->z);
-		if (isnan(p->z))
+		if (!p->z)
 			return Point(p->x, p->y, arg);
-		throw MyExeption("incorrect Point in CalcFunction::calc");
 	}
 
 	std::vector<double> calc(const Point* p)
 	{
-		//if (!check_point(p)) //DEBUG comment
-			//throw MyExeption("incorrect Point in CalcFunction.calc"); //DEBUG comment
+		if (!check_point(p))
+			throw MyExeption("incorrect Point in CalcFunction.calc");
 
 		std::vector<double> third_coord;
 		for (double arg = R.start; arg < R.end; arg += step.f)
 		{
-			if (-error_rate <= F(put_to_nan(p, arg)) && F(put_to_nan(p, arg)) <= error_rate)
+			if (F(put_to_nan(p, arg)) <= error_rate)
 				third_coord.push_back(arg);
-
 		}
 		return third_coord;
 	}		
 	
 };
 
-const ScreenPoint* get_projection(Point p)
-{
-	const ScreenPoint* sp = new ScreenPoint(0, 0);
-	return sp;
-}
+const ScreenPoint* get_projection(Point p);
 
 class Line
 {
@@ -146,22 +130,21 @@ private:
 	std::vector<const ScreenPoint *> line;
 
 public:
-	std::vector<const Point *> line_real_points; //DEBUG
 	Line(CalcFunction FF, Range rr, Point&& fa)
 		: F{FF}, R{rr}, fix_arg{fa} //?? вынести эту красивую строчку за после if?
 	{ 
-		//if (!check_point(&fix_arg)) //DEBUG comment
-			//throw MyExeption("incorrect Point in Line constructor"); //DEBUG comment
+		if (!check_point(&fix_arg))
+			throw MyExeption("incorrect Point in Line constructor");
 
 		calculate(); 
 	}
 
-	/*bool check_point(const Point* p)
+	bool check_point(const Point* p)
 	{
-		if ((!isnan(p->x) && !isnan(p->y)) || (!isnan(p->y) && !isnan(p->z)) || (!isnan(p->x) && !isnan(p->z)) || (!isnan(p->x) && !isnan(p->y) && !isnan(p->z)))
+		if ((p->x && p->y) || (p->y && p->z) || (p->x && p->z) || (p->x && p->y && p->z))
 			return false;
-		return true; //DEBUG comment
-	}*/ 
+		return true;
+	}
 
 	void calculate()
 	{
@@ -169,43 +152,34 @@ public:
 		{
 			Point p;
 			std::vector<double> third_coord;
-			if (!isnan(fix_arg.x))
+			if (fix_arg.x)
 			{
 				p.x = fix_arg.x;
 				p.y = arg;
 				third_coord = F.calc(&p);
 				for (double tc : third_coord)
-				{
 					p.z = tc;
-					line_real_points.push_back(new Point(p)); //DEBUG
 					line.push_back(get_projection(p));
-				}
 			}
 
-			else if (!isnan(fix_arg.y))
+			else if (fix_arg.y)
 			{
 				p.y = fix_arg.y;
 				p.z = arg;	
 				third_coord = F.calc(&p);
 				for (double tc : third_coord)
-				{
 					p.x = tc;
-					line_real_points.push_back(new Point(p)); //DEBUG
 					line.push_back(get_projection(p));
-				}
 			}
 
-			else if (!isnan(fix_arg.z))
+			else if (fix_arg.z)
 			{
 				p.z = fix_arg.z;
 				p.x = arg;
 				third_coord = F.calc(&p);
 				for (double tc : third_coord)
-				{
 					p.y = tc;
-					line_real_points.push_back(new Point(p)); //DEBUG
 					line.push_back(get_projection(p));
-				}
 			}		
 		}
 	}
@@ -243,19 +217,6 @@ public:
 
 	std::vector <const Line*>& get_graph() { return graph; }
 
-	void print()  //DEBUG
-	{
-		for (int nl = 0; nl < graph.size(); ++nl)
-		{
-			for(int np = 0; np < graph[nl]->line_real_points.size(); ++np)
-			{
-				std::cout << "point: (" << graph[nl]->line_real_points[np]->x \
-				<< ", "<<graph[nl]->line_real_points[np]->y\
-				<<", "<<graph[nl]->line_real_points[np]->z << ")"<<std::endl;
-			}
-		}
-	}
-
 	//объекты по указателям лежат в динамической памяти
 	~Graph()
 	{
@@ -263,3 +224,5 @@ public:
 			delete graph[l];
 	}
 };
+
+
